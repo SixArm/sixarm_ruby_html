f5795fde35e8da37055b4392e051bd2137072e7f
Joel Parker Henderson <joel@joelparkerhenderson.com>
2015-07-07 11:36:42 -0600

    Refactor single-quote to double-quote

--- test/sixarm_ruby_html_test.rb
+++ test/sixarm_ruby_html_test.rb
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
-require 'minitest/autorun'
-require 'simplecov'
+require "minitest/autorun"
+require "simplecov"
 SimpleCov.start
-require 'sixarm_ruby_html'
+require "sixarm_ruby_html"
 
 ['lists','misc','tables'].map{|x|
   require "sixarm_ruby_html_test/#{x}_test.rb"
--- test/sixarm_ruby_html_test/misc_test.rb
+++ test/sixarm_ruby_html_test/misc_test.rb
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
-require 'minitest/autorun'
-require 'sixarm_ruby_html'
+require "minitest/autorun"
+require "sixarm_ruby_html"
 
 describe HTML do
 
